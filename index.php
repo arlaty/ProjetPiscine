bfa1a02070bcdf6ec770fb2baef0275cb59e273a
Alexis <alexis.freidel@noos.fr>
2020-04-15 10:28:08 +0200

    Création de l'arboréscence des fichiers + prob link css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet"  type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <title>ECE ebay</title>
+</head>
+<body>
+    <?php include("./modules/nav.php"); ?>
+    <?php include("./modules/header.php"); ?>
+    <div>
+
+    </div>
+    <?php include("./modules/footer.php"); ?>
+</body>
+</html>
---
+++ modules/nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-md">
+    <a class="navbar-brand" href="#"><img src="./icon/logo.png" alt="logo"></a>
+    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="main-navigation">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <form action="" method="post">
+                    <input type="text">
+                    <button><img src="./icon/loupe-32.png" alt="Rechercher"></button>
+                </form>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="#">Mon Ebay</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div class="collapse navbar-collapse" id="main-navigation">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="#">Catégories</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Achat</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Vendre</a></li>
+        </ul>
+    </div>
+</nav>
