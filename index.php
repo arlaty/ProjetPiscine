cba5f883fdf6d226508b832bffbd1165222000f1
Alexis <alexis.freidel@noos.fr>
2020-04-15 18:24:00 +0200

    fix carroussel + prob taille image

--- index.php
+++ index.php
@@ -13,53 +13,57 @@
 </head>
 <body>
     <?php include("./modules/header.php"); ?>
-    <div class="container-fluid">
-        <div id="carrousel" >
-           <ul>
-               <li><img src="Images/promo1.jpg" width="1200" height="400" /></li>
-               <li><img src="Images/promo2.jpg" width="1200" height="400" /></li>
-               <li><img src="Images/promo3.jpg" width="1200" height="400" /></li>
-               <li><img src="Images/promo4.jpg" width="1200" height="400" /></li>
-               <li><img src="Images/promo5.jpg" width="1200" height="400" /></li>
-           </ul>
-       </div>
-       <input type="button" value="Précédent" class="prev">
-       <input type="button" value="Suivant" class="next">
-       <div id="cat">
-        <h4>Quelques catégories : </h4>
-        <p>
-          <a href="#">Tout afficher -></a> 
-        </p>
-        <div class="row">
-          <div class="col-lg-4 col-md-4 col-sm-12">
-            <div class="thumbnail">
-              <a href="images/categorieTresor.jpg">
-                <img src="images/categorieTresor.jpg" alt="fraise" style="width: 100%; height:300px;">
-                <div class="caption" style="text-align: left;">
-                  <p>Pour votre santé, mangez au moins 5 fruits et legumes pas jour.</p>
-                </div>
-              </a>
+    <ul class="carroussel">
+        <li>
+            <a href="#" class="prev"><img src="./icon/arrow-left.png" alt="arrow prev"></a>
+        </li>    
+        <li id="carrousel" >   
+            <ul>
+                <li><img src="Images/promo1.jpg"/></li>
+                <li><img src="Images/promo2.jpg"/></li>
+                <li><img src="Images/promo3.jpg"/></li>
+                <li><img src="Images/promo4.jpg"/></li>
+                <li><img src="Images/promo5.jpg"/></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#" class="next"><img src="./icon/arrow-right.png" alt="arrow next"></a>
+        </li>
+    </ul>
+    <div id="cat">
+    <h4>Quelques catégories : </h4>
+    <p>
+        <a href="#">Tout afficher -></a> 
+    </p>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="thumbnail">
+            <a href="images/categorieTresor.jpg">
+            <img src="images/categorieTresor.jpg" alt="fraise" style="width: 100%; height:300px;">
+            <div class="caption" style="text-align: left;">
+                <p>Pour votre santé, mangez au moins 5 fruits et legumes pas jour.</p>
             </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-12">
-            <div class="thumbnail">
-              <a href="images/categorieMusee.jpg">
-                <img src="images/categorieMusee.jpg" alt="figue" style="width: 100%; height:300px;">
-                <div class="caption" style="text-align: left;"> Pour votre santé, mangez au moins 5 fruits et legumes pas jour.</div>
-              </a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-4 col-sm-12">
-            <div class="thumbnail">
-              <a href="images/categorieAccessoireVIP.jpg">
-                <img src="images/categorieAccessoireVIP.jpg" alt="myrtille" style="width: 100%; height:300px;">
-                <div class="caption" style="text-align: left;"> Pour votre santé, mangez au moins 5 fruits et legumes pas jour.</div>
-              </a>
-            </div>
-          </div>
+            </a>
         </div>
-      </div>
-   </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="thumbnail">
+            <a href="images/categorieMusee.jpg">
+            <img src="images/categorieMusee.jpg" alt="figue" style="width: 100%; height:300px;">
+            <div class="caption" style="text-align: left;"> Pour votre santé, mangez au moins 5 fruits et legumes pas jour.</div>
+            </a>
+        </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="thumbnail">
+            <a href="images/categorieAccessoireVIP.jpg">
+            <img src="images/categorieAccessoireVIP.jpg" alt="myrtille" style="width: 100%; height:300px;">
+            <div class="caption" style="text-align: left;"> Pour votre santé, mangez au moins 5 fruits et legumes pas jour.</div>
+            </a>
+        </div>
+        </div>
+    </div>
+    </div>
    <?php include("./modules/footer.php"); ?>
 </body>
 </html>
