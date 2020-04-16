c50bb3a2ac7a81885f5a4f9b3f62be85b3d92587
Clement janot <clement.janot@edu.ece.fr>
2020-04-16 11:46:31 +0200

    nouvelle page vitrine.php

---
+++ vitrine_CA.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html >
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <title>ECE ebay</title>
+</head>
+<body>
+    <?php include("./modules/header.php"); ?>
+    
+   <div class="objet">
+       <img src="images/Piece100Francs1.jpg">
+       <p>Description</p>
+       <p class="prix">11€</p>
+   </div>
+
+    <?php include("./modules/footer.php"); ?>
+</body>
+</html>
