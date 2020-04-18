284990030d4a20a51ba8a86770b0c2ba791e56e5
LDDQ <lizziedelaisser@hotmail.com>
2020-04-18 18:14:24 +0200

    ajout de dernire minutes + remplacement guillement

--- ajout.php
+++ ajout.php
@@ -13,45 +13,45 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
     <title>Ajout d'un article</title>
-    
+
 </head>
 <body>
     <?php include("./modules/header.php"); ?>
     
-    <div class="ajout-content">
+    <div class='ajout-content'>
         <h2>Ajouter un article</h2>
-        <hr color="black" width="50%" align="center">
+        <hr color='black' width='50%' align='center'>
         <br><br>
 
-        <div class="ajout-img" >
-            <button class="button button1" onclick="on()">Ajouter une image ou une video<br></button>
+        <div class='ajout-img' >
+            <button class='button button1' onclick='on()'>Ajouter une image ou une video<br></button>
         </div>
 
         <form>
-        <div id="overlay-ajout">
-            <div id="text-ajout">
+        <div id='overlay-ajout'>
+            <div id='text-ajout'>
                 Vous pouvez ajouter jusqu'à 3 photos et une video.<br><br>
                 Charger la ou les photos ici :
-                <input type="file" id="img-file" name="img-file" multiple><br><br>
+                <input type='file' id='img-file' name='img-file' multiple><br><br>
                 Entrer le lien de la video ici :
-                <input type="text" id="lien-video" name="lien-video">
-                <br><button onclick="off()">Ajouter</button>
+                <input type='text' id='lien-video' name='lien-video'>
+                <br><button onclick='off()'>Ajouter</button>
             </div>
         </div>
             
-                <div class="infos" >
+                <div class='infos' >
                 <table>
                     <tr>
                         <td>Nom de l'article :</td>
-                        <td> <input type="text" name="nom" required></td>
+                        <td> <input type='text' name='nom' required></td>
                     </tr>
                     <tr>
                         <td><br>Description :</td>
-                            <td> <input type="text" name="desc" required></td>
+                            <td> <input type='text' name='desc' required></td>
                     </tr>
                     <tr>
                         <td><br>Sélectionner une catégorie :</td>
-                         <td> <select name="cate" id="cate" size="1">
+                         <td> <select name='cate' id='cate' size='1'>
                                 <option>Ferraille, trésors</option>
                                 <option>Bon pour le Musée</option>
                                 <option>Accessoire VIP</option>
@@ -62,17 +62,17 @@
                         <td><br>Sélectionner un type d'achat :</td>
                     </tr>
                     <tr>
-                        <td><br>Vente immédiate <input id="c1" type="checkbox" name="imm" checked onclick="check1()"></td>
-                        <td><br>Vente aux Enchères <input id="c2" type="checkbox" name="encheres" onclick="check2()"></td>                 
-                        <td><br>Meilleure offre <input id="c3" type="checkbox" name="bestoffer" onclick="check3()"> </td>
+                        <td><br>Vente immédiate <input id='c1' type='checkbox' name='imm' checked onclick='check1()'></td>
+                        <td><br>Vente aux Enchères <input id='c2' type='checkbox' name='encheres' onclick='check2()'></td>                 
+                        <td><br>Meilleure offre <input id='c3' type='checkbox' name='bestoffer' onclick='check3()'> </td>
                     </tr>
                     <tr>
                         <td><br>Prix :</td>
-                        <td> <input type="number" name="prix" step="0.01" min="1" required></td>
+                        <td> <input type='number' name='prix' step='0.01' min='1' required></td>
                     </tr>
                     <tr>
-                        <td colspan="2" align="center">
-                            <br><button id="add" type="submit">Ajouter</button>
+                        <td colspan='2' align='center'>
+                            <br><button id='add' type='submit'>Ajouter</button>
                         </td>
                     </tr>
                 </table>
@@ -84,21 +84,21 @@
 
     <script>
             function on() {
-              document.getElementById("overlay-ajout").style.display = "block";
+              document.getElementById('overlay-ajout').style.display = 'block';
             }
 
             function off() {
-              document.getElementById("overlay-ajout").style.display = "none";
+              document.getElementById('overlay-ajout').style.display = 'none';
             }
             function check1() {
-              document.getElementById("c2").checked = false;
+              document.getElementById('c2').checked = false;
             }
             function check2() { 
-              document.getElementById("c1").checked = false;
-              document.getElementById("c3").checked = false;
+              document.getElementById('c1').checked = false;
+              document.getElementById('c3').checked = false;
             }
             function check3() {
-              document.getElementById("c2").checked = false; 
+              document.getElementById('c2').checked = false; 
             }
         </script>
 
--- connexion.php
+++ connexion.php
@@ -64,29 +64,29 @@
 </head>
 <body>
 	
-	<div class="en-tete">
-        <a href="index.php"><img src="./icon/logo.png" alt="logo"class="logo"></a>
+	<div class='en-tete'>
+        <a href='index.php'><img src='./icon/logo.png' alt='logo'class='logo'></a>
     </div>
 
-    <div class="row">
-	    <div class="connexion" id="col">
+    <div class='row'>
+	    <div class='connexion' id='col'>
 	    	<h1>S'identifier</h1>
-	    	<hr color="black" width="80%">
+	    	<hr color='black' width='80%'>
 	    	<br><br>
 
-	    	<form action="traitement/connexion.php" method="post">
+	    	<form action='traitement/connexion.php' method='post'>
 	    		<table>
 		    		<tr>
 		    			<td>Identifiant / E-mail :<br></td>
-		    			<td><input type="text" name="i/e" required></td>
+		    			<td><input type='text' name='i/e' required></td>
 		    		</tr>
 		    		<tr>
 		    			<td>Mot de passe  :<br></td>
-		    			<td><input type="text" name="mdp" required></td>
+		    			<td><input type='text' name='mdp' required></td>
 		    		</tr>
 		    		<tr>
-						<td colspan="2" align="center">
-							<br><br><input type="submit" name="connect" value="Se connecter">
+						<td colspan='2' align='center'>
+							<br><br><input type='submit' name='connect' value='Se connecter'>
 						</td>
 					</tr>
 				</table>
@@ -94,46 +94,62 @@
 
 		</div>
 
-		<div class="inscription" id="col">
+		<div class='inscription' id='col'>
 			<h1>S'inscrire</h1>
-	    	<hr color="black" width="80%">
+	    	<hr color='black' width='80%'>
 	    	<br><br>
-			<?php if(isset($_GET['issues'])) echo "<p>L'utilisateur existe déjà</p>"?>
-	    	<form action="traitementInscri.php" method="post">
+			<?php if(isset($_GET["issues"])) echo "<p>L'utilisateur existe déjà</p>"?>
+	    	<form action='traitementInscri.php' method='post'>
 	    		<table>
 					<tr>
 						<td>Nom :</td>
-						<td><input type="text" name="nom" required></td>
+						<td><input type='text' name='nom' required></td>
 					</tr>
 					<tr>
 						<td>Prénom :</td>
-						<td><input type="text" name="prenom" required></td>
+						<td><input type='text' name='prenom' required></td>
 					</tr>
 					<tr>
 						<td>E-mail :</td>
-						<td><input type="text" name="email" required></td>
+						<td><input type='text' name='email' required></td>
 					</tr>
 					<tr>
 						<td>Adresse (ligne 1) :</td>
-						<td><input type="text" name="ad1" required></td>
+						<td><input type='text' name='ad1' required></td>
 					</tr>
 					<tr>
 						<td>Adresse (ligne 2) :</td>
-						<td><input type="text" name="ad2"></td>
+						<td><input type='text' name='ad2'></td>
+					</tr>
+					<tr>
+						<td>Ville :</td>
+						<td><input type='text' name='ville' required></td>
+					</tr>
+					<tr>
+						<td>Code Postal :</td>
+						<td><input type='text' name='CP' required></td>
+					</tr>
+					<tr>
+						<td>Pays :</td>
+						<td><input type='text' name='pays' required></td>
+					</tr>
+					<tr>
+						<td>Numéro de téléphone :</td>
+						<td><input type='text' name='tel' required></td>
 					</tr>
 					<tr>
 						<td><br>Type :</td>
 						<td>
-							<input type="radio" id="type1" name="type" value="acheteur" onclick="onAcheteur()" checked>
-							<label for="acheteur">Acheteur</label>
-							<input type="radio" id="type2" name="type" value="vendeur" onclick="offAcheteur()">
-							<label for="vendeur">Vendeur</label>
+							<input type='radio' id='type1' name='type' value='acheteur' onclick='onAcheteur()' checked>
+							<label for='acheteur'>Acheteur</label>
+							<input type='radio' id='type2' name='type' value='vendeur' onclick='offAcheteur()'>
+							<label for='vendeur'>Vendeur</label>
 						</td>
 					</tr>
-					<tr class="form-acheteur">
+					<tr class='form-acheteur'>
 						<td><br>Type de carte :</td>
 						<td>
-							<select name="carte" id="carte" size="1">
+							<select name='carte' id='carte' size='1'>
 								<option>Visa</option>
 								<option>MasterCard</option>
 								<option>American Express</option>
@@ -141,46 +157,46 @@
 							</select>
 						</td>
 					</tr>
-					<tr class="form-acheteur">
+					<tr class='form-acheteur'>
 						<td>Titulaire de la carte :</td>
-						<td><input type="text" id="tit" name="tit" required></td>
+						<td><input type='text' id='tit' name='tit' required></td>
 					</tr>
-					<tr class="form-acheteur">
+					<tr class='form-acheteur'>
 						<td>Numéro :</td>
-						<td><input type="number" id="num" name="num" step="1" min="00000000000000000" max="99999999999999999" required></td>
+						<td><input type='number' id='num' name='num'pattern='[0-9]{16}' required></td>
 					</tr>
-					<tr class="form-acheteur">
+					<tr class='form-acheteur'>
 						<td>Date d'expiration :</td>
-						<td><input type="month" id="exp" name="exp" required></td>
+						<td><input type='month' id='exp' name='exp' required></td>
 					</tr>
-					<tr class="form-acheteur">
+					<tr class='form-acheteur'>
 						<td>Cryptogramme :</td>
-						<td><input type="number" id="crypt" name="crypt" step="1" min="000" max="999" required></td>
+						<td><input type='number' id='crypt' name='crypt' pattern='[0-9]{3,4}' required></td>
 					</tr>
 					<tr>
 						<td><br>Identifiant :</td>
-						<td><input type="text" name="identifiant" required></td>
+						<td><input type='text' name='identifiant' required></td>
 					</tr>
 					<tr>
 						<td>Mot de passe  :</td>
-						<td><input type="text" name="mdp1" required></td>
+						<td><input type='password' name='mdp1' required></td>
 					</tr>
 					<tr>
 						<td>Confirmer le mot de passe  :</td>
-						<td><input type="text" name="mdp1" required></td>
+						<td><input type='password' name='mdp1' required></td>
 					</tr>
-					<tr class="form-acheteur">
+					<tr class='form-acheteur'>
 						<td>
 							<div>
 								<p><br>J'accepte la clause d'offre :</p>
-								<div class="clause" onclick="on()">CLAUSE *</div>
+								<div class='clause' onclick='on()'>CLAUSE *</div>
 							</div> 
 						</td>
-						<td><input id="check" type="checkbox" name="accepter" onclick="etatBtn()" required></td>
+						<td><input id='check' type='checkbox' name='accepter' onclick='etatBtn()' required></td>
 					</tr>
 		    		<tr>
-						<td colspan="2" align="center">
-							<br><button id="create" type="submit" disabled>Créer un compte</button>
+						<td colspan='2' align='center'>
+							<br><button id='create' type='submit' disabled>Créer un compte</button>
 						</td>
 					</tr>
 				</table>
@@ -188,8 +204,8 @@
 	    	</form>
 
 	    	<!--Pop up Clause d'offre-->
-	    	<div id="overlay" onclick="off()">	
-				  <div id="text"><h3>
+	    	<div id='overlay' onclick='off()'>	
+				  <div id='text'><h3>
 		    				Clause offre
 		    			</h3>
 		    			<p>
