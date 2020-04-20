c32cba42d5d5dd920a6ca90e883f5d81198244f2
Alexis <alexis.freidel@noos.fr>
2020-04-20 13:47:32 +0200

    offre pop up

--- traitement/offre.php
+++ traitement/offre.php
@@ -14,8 +14,6 @@
         array_push($_SESSION['panier']['immediat'],$id);
         header("Location: ../panier.php");
     }
-    $id=$_GET['id'];
-    $id_user=$_SESSION['id'];
     $sql="DELETE FROM `immediat` WHERE achatId=".$id." AND acheteurId=".$id_user;
     $result = mysqli_query($db_handle, $sql);
     foreach ($_SESSION['panier']['immediat'] as $key => $value) {
